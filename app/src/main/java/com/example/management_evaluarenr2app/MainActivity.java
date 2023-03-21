f836bff206efc6ff49b9a2f921662fabf839495b
Roabis Cristian <roabis.cristian@elev.cihcahul.md>
2023-03-21 10:41:15 +0200

    Creare Main activity

--- app/src/main/java/com/example/management_evaluarenr2app/MainActivity.java
+++ app/src/main/java/com/example/management_evaluarenr2app/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.management_evaluarenr2app;
 
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.management_evaluarenr2app.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                int length = editText.length();
+                String convert = String.valueOf(length);
+                textView.setText(convert);
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
     }
 }
